c2e7165314e036722a56a136c49c26cad56426ec
Michal Hoffman <michal.z.hoffman@gmail.com>
2022-07-13 18:19:34 +0100

    adding checkboxes

--- scripts.js
+++ scripts.js
@@ -2,7 +2,10 @@ var addTask = () => {
   var task = document.getElementById('taskToAdd').value
   var tasks = document.getElementById('tasks')
   var newTask = document.createElement('p')
+  task = task + '<input type="checkbox" name="done">'
   newTask.innerHTML = task
+  console.log(newTask)
+
   document.body.appendChild(newTask, tasks)
 }
 const submit = document.getElementById('addTask')
