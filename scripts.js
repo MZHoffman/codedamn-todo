e1f416b7fe3ff9d1fc97cd431ceaf7c0c7c0000e
Michal Hoffman <michal.z.hoffman@gmail.com>
2022-07-13 23:11:47 +0100

    edit function implemented

--- scripts.js
+++ scripts.js
@@ -3,11 +3,13 @@ var addTask = () => {
   var tasks = document.querySelector('.tasks')
   var newTask = document.createElement('div')
   task =
-    '<p>' +
+    '<input disabled type="text" name="editTaskField" class="editTaskField" value="' +
     task +
-    '</p><input type="checkbox" name="done" class="checkTask">' +
-    '<a href="#" class="delete">Delete</a>' +
-    '<a href="#" class="editTask">Edit</a>'
+    '"> ' +
+    '<input type="checkbox" name="done" class="checkTask">' +
+    '<button class="editTask">Edit</button>' +
+    '<input type="button" value="saveEdit" class="TaskFieldSave" hidden>' +
+    '<a href="#" class="delete">Delete</a>'
   newTask.innerHTML = task
   tasks.appendChild(newTask)
 }
@@ -22,7 +24,10 @@ var clickElement = (e) => {
       deleteTask(target)
       break
     case 'editTask':
-      editTask()
+      editTask(target)
+      break
+    case 'TaskFieldSave':
+      saveEdit(target)
       break
   }
 }
@@ -31,8 +36,26 @@ var deleteTask = (target) => {
     target.parentElement.remove()
   }
 }
-var editTask = () => {
-  console.log("i'll be editin'")
+var editTask = (target) => {
+  console.log("i'll be editin'" + target.innerHTML)
+  console.log(target.parentElement.querySelector('.editTaskField'))
+  target.parentElement
+    .querySelector('.editTaskField')
+    .removeAttribute('disabled')
+  target.parentElement.querySelector('.TaskFieldSave').removeAttribute('hidden')
+  target.parentElement
+    .querySelector('.editTask')
+    .setAttribute('hidden', 'hidden')
+}
+
+var saveEdit = (target) => {
+  target.parentElement
+    .querySelector('.editTaskField')
+    .setAttribute('disabled', 'disabled')
+  target.parentElement
+    .querySelector('.TaskFieldSave')
+    .setAttribute('hidden', 'hidden')
+  target.parentElement.querySelector('.editTask').removeAttribute('hidden')
 }
 const main = document.querySelector('.main')
 main.addEventListener('click', clickElement)
